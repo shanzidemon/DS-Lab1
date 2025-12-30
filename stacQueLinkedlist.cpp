987bad7dddb83a2ae9ceb4d7f1c147dc7dc4c7ab
shanzidemon <shanzid0001@gmail.com>
2025-12-30 13:00:03 +0600

    add.

---
+++ stacQueLinkedlist.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+Node* top = NULL;
+
+void push(int value) {
+    Node* n = new Node();
+    n->data = value;
+    n->next = top;
+    top = n;
+}
+
+void pop() {
+    if (top == NULL) {
+        cout << "Stack Empty\n";
+        return;
+    }
+    Node* del = top;
+    top = top->next;
+    delete del;
+}
+
+void displayStack() {
+    Node* temp = top;
+    cout << "Stack: ";
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+Node* front = NULL;
+Node* rear = NULL;
+
+void enqueue(int value) {
+    Node* n = new Node();
+    n->data = value;
+    n->next = NULL;
+
+    if (front == NULL) {
+        front = rear = n;
+        return;
+    }
+    rear->next = n;
+    rear = n;
+}
+
+void dequeue() {
+    if (front == NULL) {
+        cout << "Queue Empty\n";
+        return;
+    }
+    Node* del = front;
+    front = front->next;
+    delete del;
+
+    if (front == NULL)
+        rear = NULL;
+}
+
+void displayQueue() {
+    Node* temp = front;
+    cout << "Queue: ";
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main() {
+
+    // STACK
+    push(10);
+    push(20);
+    push(30);
+    displayStack();
+
+    pop();
+    displayStack();
+
+    cout << endl;
+
+
+    enqueue(1);
+    enqueue(2);
+    enqueue(3);
+    displayQueue();
+
+    dequeue();
+    displayQueue();
+
+    return 0;
+}
