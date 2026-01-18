716925e0c5e312b670671077f8f12f3f31ecea4e
shanzidemon <shanzid0001@gmail.com>
2026-01-18 23:15:59 +0600

    Change Path

--- practice402.cpp
+++ practice402.cpp
@@ -1,2 +1,34 @@
 #include<iostream>
 using namespace std;
+
+struct Node{
+    int value;
+    Node* next;
+};
+Node* top= NULL;
+
+void push(int value2){
+    Node* n=new Node();
+    n->value=value2;
+    n->next=top;
+    top=n;
+}
+
+void pop() {
+    if (top == NULL) {
+        cout << "Stack Empty\n";
+        return;
+    }
+    Node* del = top;
+    top = top->next;
+    delete del;
+}
+void displayStack() {
+    Node* temp = top;
+    cout << "Stack: ";
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
